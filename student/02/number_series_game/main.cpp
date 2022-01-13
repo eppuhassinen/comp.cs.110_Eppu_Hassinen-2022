549556e85c90e7c7230560b0c41e089a2f490e94
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-01-13 18:29:40 +0200

    Lisätty 7:llä jaollisten lukujen tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,21 +3,30 @@
 
 int main()
 {
+    // ask the user for input
     std::cout << "How many numbers would you like to have? ";
     int amount = 1;
     std::cin >> amount;
 
+    // goes through numbers
     for ( int i = 1; i <= amount; ++i )
     {
-        if (i % 3 == 0)
+        if (i % 3 == 0 && i % 7 == 0)
+        {
+            std::cout << "zip boing" << std::endl;
+        }
+        else if (i % 3 == 0)
         {
             std::cout << "zip" << std::endl;
+        }
+        else if (i % 7 == 0)
+        {
+            std::cout << "boing" << std::endl;
         }
         else
         {
             std::cout << i << std::endl;
         }
-
     }
 
     return 0;
