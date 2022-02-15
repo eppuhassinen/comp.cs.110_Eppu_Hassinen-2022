523e0019c2ce02c90811b361d27421d2140f44d1
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-02-15 08:55:58 +0200

    Program now adds points together

--- student/05/points/main.cpp
+++ student/05/points/main.cpp
@@ -19,10 +19,29 @@ int main()
     }
 
     map<string, int> points;
-    string row;
+    string row, name;
+    int new_points;
+    string::size_type splitter;
     while (getline(input_file, row))
     {
-        cout << row << endl;
+        splitter = row.find(':');
+        name = row.substr(0, splitter);
+        new_points = stoi(row.substr(splitter + 1));
+
+        if (points.find(name) == points.end())
+        {
+            points.insert({name, new_points});
+        }
+        else
+        {
+            points.at(name) += new_points;
+        }
+    }
+    input_file.close();
+    cout << "Final scores:" << endl;
+    for (const auto& i : points)
+    {
+        cout << i.first << ": " << i.second << endl;
     }
 
 
