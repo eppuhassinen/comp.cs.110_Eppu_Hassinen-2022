7fef60589cf1c0c8a26815426925b08f85e2c9a6
Eppu Hassinen <eppu.hassinen@tuni.fi>
2022-03-07 09:21:38 +0200

    Opening a file and saving the data

--- student/07/games/main.cpp
+++ student/07/games/main.cpp
@@ -11,7 +11,7 @@
  * ADD_GAME <game name> - Adds a new game
  * ADD_PLAYER <game name> <player name> <score> - Adds a new player with the
  * given score for the given game, or updates the player's score if the player
- * already playes the game
+ * already plays the game
  * REMOVE_PLAYER <player name> - Removes the player from all games
  *
  *  The data file's lines should be in format game_name;player_name;score
@@ -19,8 +19,12 @@
  *
  * */
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <utility>
+
 
 // Casual split func, if delim char is between "'s, ignores it.
 std::vector<std::string> split( const std::string& str, char delim = ';' )
@@ -49,7 +53,69 @@ std::vector<std::string> split( const std::string& str, char delim = ';' )
     return result;
 }
 
+// Opens a text file from applications build directory with user given name
+std::ifstream open_file()
+{
+    std::cout << "Give a name for input file: ";
+    std::string file_name; // name of file to be opened
+    std::getline(std::cin, file_name);
+    std::ifstream data_file(file_name); // opens the file
+    return data_file;
+}
+
+// Adds a player to the data set
+bool add_player(const std::string input_line,
+               std::map<std::string, std::map<std::string, int>>& data)
+{
+    // checks if input line has exactly 3 arguments, returns false if not
+    auto line_vector = split(input_line);
+    if (line_vector.size() != 3)
+        return false;
+
+    // If the game in "line_vector.at(0)" is not already in data set, adds it
+    if (data.find(line_vector.at(0)) == data.end())
+    {
+        std::pair<std::string, int> pair (line_vector.at(1), std::stoi(line_vector.at(2)));
+        data.insert({line_vector.at(0), {pair}});
+        return true;
+    }
+
+    // If game was already in the map, adds players to it
+    data.at(line_vector.at(0)).insert({line_vector.at(1), std::stoi(line_vector.at(2))});
+    return true;
+}
+
 int main()
 {
+    // Data structure to save games, players and scores
+    std::map<std::string, std::map<std::string, int>> data;
+    // Opens a text file
+    auto file = open_file(); // returns false if the file couldn't be opened
+    if (not file)
+    {
+        std::cout << "Error: File could not be read." << std::endl;
+        return EXIT_FAILURE;
+    }
+    // Goes through line by line and saves the information to "data"
+    std::string row;
+    while (getline(file, row))
+    {
+        if (!add_player(row, data))
+        {
+            std::cout << "Error: Invalid format in file." << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+
+    // for testing
+    for (auto& game : data)
+    {
+
+        for (auto& row : game.second)
+        {
+            std::cout << game.first << " : " << row.first << " : " << row.second << std::endl;
+        }
+    }
+
     return EXIT_SUCCESS;
 }
